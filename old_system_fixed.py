548256d6a614094753ad69703c955181132025f3
j-s-s-10 <jss10@kent.ac.uk>
2026-02-11 18:02:20 +0000

    Fixed bug 8

--- old_system_fixed.py
+++ old_system_fixed.py
@@ -46,11 +46,15 @@ def run_system_monolith():
         elif opt == "3":
             rem = input("Name to remove: ")
            
-            idx = n.index(rem)
-            n.pop(idx)
-            r.pop(idx)
-            d.pop(idx)
-            print("Removed.")
+            try:
+                idx = n.index(rem)
+            except ValueError:
+                print("Invalid name")
+            else:
+                n.pop(idx)
+                r.pop(idx)
+                d.pop(idx)
+                print("Removed.")
             
         elif opt == "4":
             print("Analyzing...")

199ebadc6db9c79060db28cbb36914bef82db061
j-s-s-10 <jss10@kent.ac.uk>
2026-02-13 14:09:53 +0000

    Implemented 'add_member'

--- fleet_manager.py
+++ fleet_manager.py
@@ -7,7 +7,43 @@ def init_database():
     return names, ranks, divisions, ids
 
 
+def add_member():
+    name = input("Enter name: ")
+    rank = input("Enter rank: ")
+    division = input("Enter division name: ")
+    try:
+        new_id = int(input("Enter a new unique ID number: "))
+    except ValueError:
+        print("Invalid ID")
+        return
+
+    if new_id in ids:
+        print("Invalid ID")
+        return
+    if rank not in ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Junior Lieutenant", "Ensign"]:
+        print("Invalid rank")
+        return
+    
+    names.append(name)
+    ranks.append(rank)
+    divisions.append(division)
+    ids.append(new_id)
+
+
+def display_menu():
+    name = input("Enter your name: ")
+    print("Current user:", name)
+
+    pass
+
+
 def main():
+    global names
+    global ranks
+    global divisions
+    global ids
+
     names, ranks, divisions, ids = init_database()
+    print(names, "\n", ranks, "\n", divisions, "\n", ids, sep = "")
 
 main()

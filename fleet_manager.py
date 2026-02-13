7451dd6b979ac31fe0f572d855a2072702692ffb
j-s-s-10 <jss10@kent.ac.uk>
2026-02-13 13:15:37 +0000

    Implemented 'init_database'

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,4 +1,13 @@
+def init_database():
+    names = ["Picard", "Riker", "Data", "Worf", "Archer"]
+    ranks = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Captain"]
+    divisions = ["Command", "Command", "Operations", "Security", "Command"]
+    ids = [1, 2, 3, 4, 5]
+
+    return names, ranks, divisions, ids
+
+
 def main():
-    pass
+    names, ranks, divisions, ids = init_database()
 
 main()
